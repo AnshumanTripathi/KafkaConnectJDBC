e4e85b04ed21097b5b66597e226fc12ed355bd6c
AnshumanTripathi <anshuman.tripathi305@gmail.com>
2018-11-19 09:48:46 -0800

    Adding db connection url and params to config

--- src/main/java/com/kafkaconnect/source/connector/JDBCSourceConnectorContext.java
+++ src/main/java/com/kafkaconnect/source/connector/JDBCSourceConnectorContext.java
@@ -10,69 +10,79 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class JDBCSourceConnectorContext {
-    private static final Logger logger = Logger.getLogger(JDBCSourceConnectorContext.class.getName());
-    private static final String CONNECTION_PARAMAS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
-    private static final String CONNECTION_URL = JDBCConnectorConfig.getConnectionUrl() +
-            JDBCConnectorConfig.getDatabase() + CONNECTION_PARAMAS;
+  private static final Logger logger = Logger.getLogger(JDBCSourceConnectorContext.class.getName());
 
-    private final AtomicBoolean isClosed = new AtomicBoolean();
+  private final AtomicBoolean isClosed = new AtomicBoolean();
+  private final JDBCConnectorConfig config;
 
-    private final Connection conn;
-    private long lastPolledId;
+  private final Connection conn;
+  private long lastPolledId;
 
-    JDBCSourceConnectorContext(final Map<String, String> props) {
-        this.lastPolledId = 0L;
-        isClosed.set(true);
-        conn = connect(props);
+  JDBCSourceConnectorContext (final Map<String, String> props) {
+    this(new JDBCConnectorConfig(props));
+  }
+
+  JDBCSourceConnectorContext(final JDBCConnectorConfig config) {
+    this.lastPolledId = 0L;
+    this.config = config;
+    isClosed.set(true);
+    this.conn = connect();
+  }
+
+  private Connection connect() {
+    if (this.conn != null) {
+      return this.conn;
     }
 
-    private Connection connect(Map<String, String> props)  {
-        Connection conn = null;
-        try {
-            Class.forName(DRIVER_CLASS_NAME);
-            String url = props.get(JDBCConnectorConfig.getConnectionUrl()) +
-                    props.get(JDBCConnectorConfig.getDatabase()) + CONNECTION_PARAMAS;
-            logger.info("Connection URL" + url);
-            conn = DriverManager.getConnection(url, props.get(JDBCConnectorConfig.getConnectionUser()),
-                    props.get(JDBCConnectorConfig.getConnectionPassword()));
-            isClosed.set(false);
-        } catch (ClassNotFoundException | SQLException e) {
-            logger.error("Exception encountered while connecting to database.", e);
-        }
-        return conn;
+    Connection conn = null;
+    try {
+      Class.forName(config.getString(JDBCConnectorConfig.getJdbcConnectorDriverClass()));
+      final String url = getConnectionUrl(config);
+      conn = DriverManager.getConnection(url, config.getString(JDBCConnectorConfig.getConnectionUser()),
+          config.getPassword(JDBCConnectorConfig.getConnectionPassword()).value());
+      isClosed.set(false);
+    } catch (ClassNotFoundException | SQLException e) {
+      logger.error("Exception encountered while connecting to database.", e);
     }
+    return conn;
+  }
 
-    public void disconnect() {
-        isClosed.set(true);
-        try {
-            conn.close();
-        } catch (SQLException e) {
-            logger.error("Exception encountered while closing database connection");
-        }
+  void disconnect() {
+    isClosed.set(true);
+    try {
+      conn.close();
+    } catch (SQLException e) {
+      logger.error("Exception encountered while closing database connection");
     }
+  }
 
-    Connection getConnection() throws SQLException {
-        if (conn == null) {
-            throw new SQLException("Failed to create a successfull mysql connection");
-        }
-        return conn;
+  Connection getConnection() throws SQLException {
+    if (conn == null) {
+      throw new SQLException("Failed to create a successfull mysql connection");
     }
+    return conn;
+  }
 
-    Boolean isClosed() {
-        return isClosed.get();
-    }
+  Boolean isClosed() {
+    return isClosed.get();
+  }
 
-    void closeConnection() {
-        isClosed.set(true);
-    }
+  void closeConnection() {
+    isClosed.set(true);
+  }
 
-    long getLastPolledId() {
-        return lastPolledId;
-    }
+  long getLastPolledId() {
+    return lastPolledId;
+  }
 
-    void setLastPolledId(long lastPolledId) {
-        this.lastPolledId = lastPolledId;
-    }
+  void setLastPolledId(long lastPolledId) {
+    this.lastPolledId = lastPolledId;
+  }
+
+  private String getConnectionUrl(final JDBCConnectorConfig config) {
+    return config.getString(JDBCConnectorConfig.getConnectionUrl()) +
+        config.getString(JDBCConnectorConfig.getDatabase()) +
+        config.getString(JDBCConnectorConfig.getConnectionUrlParams());
+  }
 
 }
--- src/main/java/com/kafkaconnect/source/connector/JDBCSourceTask.java
+++ src/main/java/com/kafkaconnect/source/connector/JDBCSourceTask.java
@@ -1,19 +1,15 @@
 package com.kafkaconnect.source.connector;
 
-import com.kafkaconnect.source.models.Person;
 import com.kafkaconnect.configs.JDBCConnectorConfig;
+import com.kafkaconnect.source.models.Person;
 import com.kafkaconnect.source.schema.PersonSchema;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.apache.log4j.Logger;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.ResultSet;
-
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,81 +17,82 @@ import java.util.Map;
 
 
 public class JDBCSourceTask extends SourceTask {
-    private static final Logger logger = Logger.getLogger(JDBCSourceTask.class.getName());
-    private Map<String, String> props;
-    private JDBCSourceConnectorContext context;
+  private static final Logger logger = Logger.getLogger(JDBCSourceTask.class.getName());
+  private Map<String, String> props;
+  private JDBCSourceConnectorContext context;
 
-    /**
-     * @return implementation version of Task
-     */
-    @Override
-    public String version() {
-        return JDBCSourceTask.class.getPackage().getImplementationVersion();
+  /**
+   * @return implementation version of Task
+   */
+  @Override
+  public String version() {
+    return JDBCSourceTask.class.getPackage().getImplementationVersion();
+  }
+
+  @Override
+  public void start(Map<String, String> props) {
+    this.props = props;
+    this.context = new JDBCSourceConnectorContext(props);
+  }
+
+  @Override
+  public List<SourceRecord> poll() {
+    final ArrayList<Person> people = new ArrayList<>();
+    try {
+      final ResultSet rs = context.getConnection().createStatement()
+          .executeQuery(String.format("SELECT * FROM person WHERE id > %s", context.getLastPolledId()));
+      while (rs.next()) {
+        final Person person = new Person(rs.getInt("id"), rs.getString("first_name"));
+        people.add(person);
+      }
+    } catch (SQLException e) {
+      logger.error("Exception encountered while fetching records from the database", e);
     }
+    return generateSourceRecords(people);
+  }
 
-    @Override
-    public void start(Map<String, String> props) {
-        this.props = props;
-        this.context = new JDBCSourceConnectorContext(props);
+
+  @Override
+  public void stop() {
+    context.closeConnection();
+    context.disconnect();
+  }
+
+  private List<SourceRecord> generateSourceRecords(List<Person> people) {
+    final ArrayList<SourceRecord> records = new ArrayList<>();
+    if (!context.isClosed()) {
+      final HashMap<String, String> sourcePartitionMap = new HashMap<>();
+      sourcePartitionMap.put("table", props.get("table"));
+      final HashMap<String, String> sourceOffsetMap = new HashMap<>();
+      sourceOffsetMap.put("Id", JDBCConnectorConfig.getID());
+
+      for (Person person : people) {
+        context.setLastPolledId(person.getId());
+        records.add(new SourceRecord(
+            sourcePartitionMap,
+            sourceOffsetMap,
+            JDBCConnectorConfig.getTOPIC(),
+            null,
+            PersonSchema.PERSON_SCHEMA_KEY,
+            buildKey(person),
+            PersonSchema.PERSON_SCHEMA_VALUE,
+            buildRecordValue(person)
+        ));
+      }
     }
+    return records;
 
-    @Override
-    public List<SourceRecord> poll() throws InterruptedException {
-        final ArrayList<Person> people = new ArrayList<>();
-        try {
-            final ResultSet rs = context.getConnection().createStatement().executeQuery(String.format("SELECT * FROM person WHERE id > %s", context.getLastPolledId()));
-            while (rs.next()) {
-                final Person person = new Person(rs.getInt("id"), rs.getString("first_name"));
-                people.add(person);
-            }
-        } catch (SQLException e) {
-            logger.error("Exception encountered while fetching records from the database", e);
-        }
-        return generateSourceRecords(people);
-    }
+  }
 
 
-    @Override
-    public void stop() {
-        context.closeConnection();
-        context.disconnect();
-    }
+  private Struct buildKey(Person person) {
+    return new Struct(PersonSchema.PERSON_SCHEMA_KEY)
+        .put("id", (person.getId()));
+  }
 
-    private List<SourceRecord> generateSourceRecords(List<Person> people) {
-        final ArrayList<SourceRecord> records = new ArrayList<>();
-        if (!context.isClosed()) {
-            final HashMap<String, String> sourcePartitionMap = new HashMap<>();
-            sourcePartitionMap.put("table", props.get("table"));
-            final HashMap<String, String> sourceOffsetMap = new HashMap<>();
-            sourceOffsetMap.put("Id", JDBCConnectorConfig.getID());
-
-            for (Person person : people) {
-                context.setLastPolledId(person.getId());
-                records.add(new SourceRecord(
-                        sourcePartitionMap,
-                        sourceOffsetMap,
-                        JDBCConnectorConfig.getTOPIC(),
-                        null,
-                        PersonSchema.PERSON_SCHEMA_KEY,
-                        buildKey(person),
-                        PersonSchema.PERSON_SCHEMA_VALUE,
-                        buildRecordValue(person)
-                ));
-            }
-        }
-        return records;
-
-    }
-
-
-    private Struct buildKey(Person person) {
-        return new Struct(PersonSchema.PERSON_SCHEMA_KEY)
-                .put("id", (person.getId()));
-    }
-
-    private Struct buildRecordValue(Person person) {
-        return new Struct(PersonSchema.PERSON_SCHEMA_VALUE)
-                .put(PersonSchema.getID(), (person.getId()))
-                .put(PersonSchema.getNAME(), person.getFirstName());
-    }
+  private Struct buildRecordValue(Person person) {
+    return new Struct(PersonSchema.PERSON_SCHEMA_VALUE)
+        .put(PersonSchema.getID(), (person.getId()))
+        .put(PersonSchema.getNAME(), person.getFirstName());
+  }
 }
